8b5294c2c3047a9bdb92b6b6e59089109711fbd2
Logan Sevcik <logan@sevcik.email>
2021-12-05 22:58:28 -0800

    Fix the last memory leaks I hope

--- mainwindow_conferences.cpp
+++ mainwindow_conferences.cpp
@@ -8,7 +8,8 @@ void MainWindow::constructConferencesTab()
     sModel->clear();
     auto root = sModel->invisibleRootItem();
     auto flags = Qt::ItemIsSelectable | Qt::ItemIsEnabled;
-    auto conferencesModel = m_controller->getStadiumsDataQueryModel(
+    QSqlQueryModel conferencesModel;
+    conferencesModel.setQuery(
         "SELECT DISTINCT [Conference] FROM [Stadiums];");
 
     /* Add the "Any" item */
@@ -20,21 +21,23 @@ void MainWindow::constructConferencesTab()
     auto divisionNode = new QStandardItem(division);
     divisionNode->setFlags(flags);
     conferenceNode->appendRow(divisionNode);
-    auto teamsModel = m_controller->getStadiumsDataQueryModel(
+    QSqlQueryModel teamsModel;
+    teamsModel.setQuery(
         "SELECT [Team Name] FROM [Stadiums];");
-    for (int i = 0; i < teamsModel->rowCount(); ++i) {
-        auto teamName = teamsModel->record(i).value(0).toString();
+    for (int i = 0; i < teamsModel.rowCount(); ++i) {
+        auto teamName = teamsModel.record(i).value(0).toString();
         auto teamNameNode = new QStandardItem(teamName);
         teamNameNode->setFlags(flags);
         divisionNode->appendRow(teamNameNode);
     }
 
-    for (int i = 0; i < conferencesModel->rowCount(); ++i) {
-        auto conference = conferencesModel->record(i).value(0).toString();
+    for (int i = 0; i < conferencesModel.rowCount(); ++i) {
+        auto conference = conferencesModel.record(i).value(0).toString();
         auto conferenceNode = new QStandardItem(conference);
         conferenceNode->setFlags(flags);
         root->appendRow(conferenceNode);
-        auto divisionsModel = m_controller->getStadiumsDataQueryModel(
+        QSqlQueryModel divisionsModel;
+        divisionsModel.setQuery(
             "SELECT DISTINCT [Division] FROM [Stadiums] WHERE [Conference] = '"+ conference +"';");
 
         /* Add the "Any item" */
@@ -42,27 +45,29 @@ void MainWindow::constructConferencesTab()
         auto divisionNode = new QStandardItem(division);
         divisionNode->setFlags(flags);
         conferenceNode->appendRow(divisionNode);
-        auto teamsModel = m_controller->getStadiumsDataQueryModel(
+        QSqlQueryModel teamsModel;
+        teamsModel.setQuery(
             "SELECT [Team Name] FROM [Stadiums] WHERE "
             "[Conference] = '" + conference + "';");
-        for (int i = 0; i < teamsModel->rowCount(); ++i) {
-            auto teamName = teamsModel->record(i).value(0).toString();
+        for (int i = 0; i < teamsModel.rowCount(); ++i) {
+            auto teamName = teamsModel.record(i).value(0).toString();
             auto teamNameNode = new QStandardItem(teamName);
             teamNameNode->setFlags(flags);
             divisionNode->appendRow(teamNameNode);
         }
 
-        for (int i = 0; i < divisionsModel->rowCount(); ++i) {
-            auto division = divisionsModel->record(i).value(0).toString();
+        for (int i = 0; i < divisionsModel.rowCount(); ++i) {
+            auto division = divisionsModel.record(i).value(0).toString();
             auto divisionNode = new QStandardItem(division);
             divisionNode->setFlags(flags);
             conferenceNode->appendRow(divisionNode);
-            auto teamsModel = m_controller->getStadiumsDataQueryModel(
+            QSqlQueryModel teamsModel;
+            teamsModel.setQuery(
                 "SELECT [Team Name] FROM [Stadiums] WHERE "
                 "[Conference] = '" + conference + "' AND "
                 "[Division] = '" + division + "';");
-            for (int i = 0; i < teamsModel->rowCount(); ++i) {
-                auto teamName = teamsModel->record(i).value(0).toString();
+            for (int i = 0; i < teamsModel.rowCount(); ++i) {
+                auto teamName = teamsModel.record(i).value(0).toString();
                 auto teamNameNode = new QStandardItem(teamName);
                 teamNameNode->setFlags(flags);
                 divisionNode->appendRow(teamNameNode);
